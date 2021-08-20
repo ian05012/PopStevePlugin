bec4379b1d45ca4d4ad8261a8630e55319a40e8a
TinyYana <yanasakuranight@gmail.com>
2021-08-20 18:51:35 +0800

    Initial commit

---
+++ src/main/java/io/github/tinyyana/popsteve/PluginMain.java
@@ -0,0 +1,17 @@
+package io.github.tinyyana.popsteve;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
